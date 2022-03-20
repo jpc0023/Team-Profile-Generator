c8713c58c7590bdf8ff6ef7ace8e13955fd14e96
jpc0023 <91299930+jpc0023@users.noreply.github.com>
2022-03-20 16:13:12 -0400

    engineer role

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,22 @@
+const { test, expect } = require('@jest/globals');
+const { ensureExpectedIsNonNegativeInteger } = require('jest-matcher-utils');
+const Employee = require('../lib/Employee.js');
 const Engineer = require('../lib/Engineer.js');
+
+test('create github username', () => {
+    const testGithub = "github";
+    const employeeCreate = new Engineer('Sally', 1, 'test@test.com', testGithub);
+    expect(employeeCreate.github).toBe(testGithub);
+})
+
+test('getGithub() module', () => {
+    const testGithub = 'testtestgithub';
+    const employeeCreate = new Engineer('Sally', 1, 'test@test.com', 'testtestgithub');
+    expect(employeeCreate.getGithub()).toBe('testtestgithub');
+})
+
+test('getRole() module', () => {
+    const role = 'Engineer';
+    const employeeCreate = new Engineer('Sally', 1, 'test@test.com', 2202, 'testtestgithub');
+    expect(employeeCreate.getRole()).toBe('Engineer');
+})
